ae37df5d7249424e424b9acb0a8ed8b03e0d3a71
William Adi Wibowo <william.adi92@gmail.com>
2025-07-18 14:43:46 +0700

    config: setup vitest and react testing library for unit test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.js",
+  },
+} as UserConfig);
